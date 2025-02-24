bb9770c87b286427ef618a35cdaeb7cc71364a85
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2025-02-24 16:38:13 +0100

    Update BoissonTest.java

--- src/test/java/fr/usmb/vpub/BoissonTest.java
+++ src/test/java/fr/usmb/vpub/BoissonTest.java
@@ -5,35 +5,30 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 class BoissonTest {
-    private String nom="Coca";
-    private Float degree= 12.0F;
-
-    protected void setUp(){
-        Boisson boisson = new Boisson(nom);
-    }
+    private final String nom="Coca";
 
     @Test
-    public void CreationBoissonNonAlcoolise(){
+    void CreationBoissonNonAlcoolise(){
         Boisson boisson = new Boisson(nom);
         assertEquals(nom,boisson.toString());
     }
 
     @Test
-    public void CreationBoissonAlcoolise(){
-        Boisson boisson = new Boisson(nom,degree);
+    void CreationBoissonAlcoolise(){
+        Boisson boisson = new Boisson(nom, 12.0F);
         assertEquals(nom + " (l'abus d'alcool est dangereux pour la sante)",boisson.toString());
     }
 
     @Test
-    public void CreationBoisson0Degrees(){
+    void CreationBoisson0Degrees(){
         Boisson boisson = new Boisson(nom,0.0F);
         assertEquals(nom, boisson.toString());
     }
 
     @Test
-    public void CreationBoissonAntiAlcoolise(){
+    void CreationBoissonAntiAlcoolise(){
         assertThrows(IllegalArgumentException.class, () -> {
-            Boisson boisson = new Boisson(nom,-1.0F);
+            new Boisson(nom,-1.0F);
         });
     }
 }
